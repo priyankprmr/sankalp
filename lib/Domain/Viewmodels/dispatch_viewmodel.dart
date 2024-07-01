86895a896c4ba3adbdb3e3cdbde3d6713577e572
Priyank <parmarpriyank215@gmail.com>
2024-07-01 13:34:07 +0530

    cmt

--- lib/Domain/Viewmodels/dispatch_viewmodel.dart
+++ lib/Domain/Viewmodels/dispatch_viewmodel.dart
@@ -1,3 +1,15 @@
+import 'package:sankalp/Domain/Models/get_pack_data_response.dart';
+import 'package:sankalp/Domain/Services/dispatch_service.dart';
+import 'package:sankalp/Utils/api_result.dart';
+
 class DispatchViewmodel {
-  
+  Future<GetPackDataResponse?> getPackData({required String barcode}) async {
+    final response = await DispatchService.getPackData(barcode: barcode);
+    if (response is Success<GetPackDataResponse>) {
+      return response.response;
+    }else{
+    return null;
+
+    }
+  }
 }

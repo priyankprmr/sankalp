80a813adf9fa1ec1890264ea9abd1634bfedfcea
Priyank <parmarpriyank215@gmail.com>
2024-07-01 15:08:52 +0530

    cmt

--- lib/Domain/Models/Dispatch/create_dispatch_data.dart
+++ lib/Domain/Models/Dispatch/create_dispatch_data.dart
@@ -1,37 +1,22 @@
 import 'dart:collection';
-
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 class CreateDispatchData extends MapView<String, dynamic> {
-  final int type;
-  final int orderId;
-  final int invoiceNo;
-  final String partyName;
-  final String barcodeScan;
-  final int id;
-  final Iterable<String> item;
-  final Iterable<int> intItemId;
-  final Iterable<int> qty;
-  final Iterable<String> caseNo;
-  final Iterable<String> batchNumber;
-  final String note;
-  final String mode;
-  
   CreateDispatchData({
-    required this.type,
-    required this.orderId,
-    required this.invoiceNo,
-    required this.partyName,
-    required this.barcodeScan,
-    required this.id,
-    required this.item,
-    required this.intItemId,
-    required this.qty,
-    required this.caseNo,
-    required this.batchNumber,
-    required this.note,
-    required this.mode,
+    required int type,
+    required int orderId,
+    required int invoiceNo,
+    required String partyName,
+    required String barcodeScan,
+    required int id,
+    required Iterable<String> item,
+    required Iterable<int> intItemId,
+    required Iterable<int> qty,
+    required Iterable<String> caseNo,
+    required Iterable<String> batchNumber,
+    required String note,
+    required String mode,
   }) : super(
           {
             "type": type,
